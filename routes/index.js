b93741c9d423177024b0bec128caa336041eb02d
aman_d1996 <aman_d1996@yahoo.com>
2016-12-14 12:13:59 +0530

    defined the structure

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/landing', function(req, res) {
+  res.render('landing');
+});
+
+router.get('/generic', function(req, res) {
+  res.render('generic');
+});
+
+router.get('/elements', function(req, res) {
+  res.render('elements');
 });
 
 module.exports = router;
