53627dce77dfe9d208c160944dc76838fb9bfbec
sakshi1996 <sakshiranka9@gmail.com>
2016-12-17 15:21:59 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -24,4 +24,7 @@ router.get('/pavitrashetty', function(req, res) {
 router.get('/arvindpassey', function(req, res) {
   res.render('arvindpassey');
 });
+router.get('/raul', function(req, res) {
+  res.render('raul');
+});
 module.exports = router;
